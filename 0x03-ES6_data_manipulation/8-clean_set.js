d14c50e349bf515422e69285ac415805716c3939
Ekarika Nsemeke <hamatthan@gmail.com>
2023-08-03 13:25:10 +0100

    correction to task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,22 +6,19 @@
  * of each item in the set.
  * @returns {String}
  */
-
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
-
-  const resultArray = [];
-  for (const value of set) {
-    if (value.startsWith(startString)) {
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       const valueSubStr = value.substring(startString.length);
 
       if (valueSubStr && valueSubStr !== value) {
-        resultArray.push(valueSubStr);
+        parts.push(valueSubStr);
       }
     }
   }
-
-  return resultArray.join('-');
+  return parts.join('-');
 }
