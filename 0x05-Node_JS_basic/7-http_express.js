3f91695825ecb220a2b9882eb04a2e95bd0f33b9
Ekarika Nsemeke <hamatthan@gmail.com>
2023-09-21 04:14:49 +0100

    Create a more complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const DATABASE = args[0];
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const msg = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(DATABASE);
+    res.send(`${msg}${students}`);
+  } catch (error) {
+    res.send(`${error.message}`);
+  }
+});
+
+app.listen(port, () => {
+  // console.log(`app listening at http://localhost:${port}`);
+});
